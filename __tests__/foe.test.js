e87e450798073caba2e954c6f18206c84cbb6e8d
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-22 18:06:31 -0700

    checkpoint! working on getting foe post route working!

--- __tests__/foe.test.js
+++ __tests__/foe.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Foe from '../lib/models/Foe.js';
+// import Foe from '../lib/models/Foe.js';
 
 describe('foe CRUD routes', () => {
   beforeEach(() => {

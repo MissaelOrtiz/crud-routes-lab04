6835247a4a8eee586158027b6f770043bca0c30d
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-23 14:22:26 -0700

    init drink test, model, and controller; created drink test via POST

---
+++ __tests__/drink.test.js
@@ -0,0 +1,18 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Drink from '../lib/models/Drink.js';
+
+describe('drink CRUD routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a drink via POST', async () => {
+    const drink = { name: 'mead', flavor: 'sweet', adult: true };
+    const res = await request(app).post('/api/v1/drinks').send(drink);
+
+    expect(res.body).toEqual({ id: '1', ...drink });
+  });
+});

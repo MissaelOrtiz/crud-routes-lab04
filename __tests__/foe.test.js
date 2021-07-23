c19948bb1582433ca432608d10c958c85c507bda
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-22 17:29:10 -0700

    added foe post test

--- __tests__/foe.test.js
+++ __tests__/foe.test.js
@@ -2,9 +2,17 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Foe from '../lib/models/Foe.js';
 
-describe('demo routes', () => {
+describe('foe CRUD routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a foe via POST', async () => {
+    const foe = { name: 'goblin', level: 1 };
+    const res = await (await request(app).post('/api/v1/foes')).setEncoding(foe);
+
+    expect(res.body).toEqual({ id: '1', ...foe });
   });
 });

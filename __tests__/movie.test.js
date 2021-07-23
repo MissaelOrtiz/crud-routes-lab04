e7952fe34c3637fa74181296629d93a7f74fdf91
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-23 16:48:25 -0700

    added movie get by id test

--- __tests__/movie.test.js
+++ __tests__/movie.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Movie from '../lib/models/Movie';
+import Movie from '../lib/models/Movie';
 
 describe('movie CRUD routes', () => {
   beforeEach(() => {
@@ -14,5 +14,12 @@ describe('movie CRUD routes', () => {
     const res = await request(app).post('/api/v1/movies').send(movie);
 
     expect(res.body).toEqual({ id: '1', ...movie });
+  });
+
+  it('gets a movie by id via GET', async () => {
+    const movie = await Movie.insert({ name: 'finding nemo', genre: 'animated/family' });
+    const res = await request(app).get(`/api/v1/movies/${movie.id}`);
+
+    expect(res.body).toEqual(movie);
   });
 });

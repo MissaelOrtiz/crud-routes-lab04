ef8d0cbd7b879aa399c2b14bc1fc4115affb9a4d
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-23 11:24:25 -0700

    added delete test via DELETE

--- __tests__/foe.test.js
+++ __tests__/foe.test.js
@@ -4,10 +4,12 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import Foe from '../lib/models/Foe.js';
 
-describe('foe CRUD routes', () => {
+describe('foe CRUD routes', async () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  const foe = await Foe.insert({ name: 'goblin', level: 1 });
 
   it('creates a foe via POST', async () => {
     const foe = { name: 'goblin', level: 1 };
@@ -17,25 +19,33 @@ describe('foe CRUD routes', () => {
   });
 
   it('gets a foe by id via GET', async () => {
-    const foe = await Foe.insert({ name: 'goblin', level: 1 });
+    foe;
     const res = await request(app).get(`/api/v1/foes/${foe.id}`);
 
     expect(res.body).toEqual(foe);
   });
 
   it('gets all foes via GET', async () => {
-    const foe1 = await Foe.insert({ name: 'goblin', level: 1 });
+    foe;
     const foe2 = await Foe.insert({ name: 'hobgoblin', level: 2 });
     const foe3 = await Foe.insert({ name: 'dire wolf', level: 3 });
     const res = await request(app).get('/api/v1/foes');
 
-    expect(res.body).toEqual([foe1, foe2, foe3]);
+    expect(res.body).toEqual([foe, foe2, foe3]);
   });
+
   it('updates a foe by id via PUT', async () => {
     const hob = { name: 'hobgoblin', level: 1 };
-    const foe = await Foe.insert({ name: 'goblin', level: 1 });
+    foe;
     const res = await request(app).put(`/api/v1/foes/${foe.id}`).send(hob);
 
     expect(res.body).toEqual({ id: '1', ...hob });
+  });
+
+  it('deletes a foe by id via DELETE', async () => {
+    foe;
+    const res = await request(app).delete(`/api/v1/foes/${foe.id}`);
+
+    expect(res.body).toEqual({ message: `This ${foe.name} has been vanquished!` });
   });
 });

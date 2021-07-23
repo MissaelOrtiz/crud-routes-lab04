daeb1ee9010f1681e4b89d6d7565e4cf6dde4adc
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-23 10:37:28 -0700

    working foe Post test

--- __tests__/foe.test.js
+++ __tests__/foe.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Foe from '../lib/models/Foe.js';
+import Foe from '../lib/models/Foe.js';
 
 describe('foe CRUD routes', () => {
   beforeEach(() => {
@@ -11,7 +11,7 @@ describe('foe CRUD routes', () => {
 
   it('creates a foe via POST', async () => {
     const foe = { name: 'goblin', level: 1 };
-    const res = await (await request(app).post('/api/v1/foes')).setEncoding(foe);
+    const res = await request(app).post('/api/v1/foes').send(foe);
 
     expect(res.body).toEqual({ id: '1', ...foe });
   });

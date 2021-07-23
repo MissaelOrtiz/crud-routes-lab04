f5547870b025cbf7a36b4b4f056b56a66ef6427a
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-23 11:31:34 -0700

    add working DELETE route and ALL TEST ARE GREEN FOR FOES!!!!!

--- __tests__/foe.test.js
+++ __tests__/foe.test.js
@@ -4,12 +4,10 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import Foe from '../lib/models/Foe.js';
 
-describe('foe CRUD routes', async () => {
+describe('foe CRUD routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
-  const foe = await Foe.insert({ name: 'goblin', level: 1 });
 
   it('creates a foe via POST', async () => {
     const foe = { name: 'goblin', level: 1 };
@@ -19,14 +17,14 @@ describe('foe CRUD routes', async () => {
   });
 
   it('gets a foe by id via GET', async () => {
-    foe;
+    const foe = await Foe.insert({ name: 'goblin', level: 1 });
     const res = await request(app).get(`/api/v1/foes/${foe.id}`);
 
     expect(res.body).toEqual(foe);
   });
 
   it('gets all foes via GET', async () => {
-    foe;
+    const foe = await Foe.insert({ name: 'goblin', level: 1 });
     const foe2 = await Foe.insert({ name: 'hobgoblin', level: 2 });
     const foe3 = await Foe.insert({ name: 'dire wolf', level: 3 });
     const res = await request(app).get('/api/v1/foes');
@@ -36,14 +34,14 @@ describe('foe CRUD routes', async () => {
 
   it('updates a foe by id via PUT', async () => {
     const hob = { name: 'hobgoblin', level: 1 };
-    foe;
+    const foe = await Foe.insert({ name: 'goblin', level: 1 });
     const res = await request(app).put(`/api/v1/foes/${foe.id}`).send(hob);
 
     expect(res.body).toEqual({ id: '1', ...hob });
   });
 
   it('deletes a foe by id via DELETE', async () => {
-    foe;
+    const foe = await Foe.insert({ name: 'goblin', level: 1 });
     const res = await request(app).delete(`/api/v1/foes/${foe.id}`);
 
     expect(res.body).toEqual({ message: `This ${foe.name} has been vanquished!` });

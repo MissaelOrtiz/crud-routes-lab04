4732fa678d8bc85b93a2ec0f74d032effeb47821
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-26 09:40:03 -0700

    added movies put test

--- __tests__/movie.test.js
+++ __tests__/movie.test.js
@@ -24,10 +24,18 @@ describe('movie CRUD routes', () => {
   });
 
   it('gets all movies via GET', async () => {
-    const movie1 = await Movie.insert({ name: 'The Ring', genre: 'horro' });
-    const movie2 = await Movie.insert({ name: 'Night of the Living Dead', genre: 'horro' });
+    const movie1 = await Movie.insert({ name: 'The Ring', genre: 'horror' });
+    const movie2 = await Movie.insert({ name: 'Night of the Living Dead', genre: 'horror' });
     const res = await request(app).get('/api/v1/movies');
 
     expect(res.body).toEqual([movie1, movie2]);
+  });
+
+  it('updates a movie by id via PUT', async () => {
+    const newMovie = { name: 'The Ring 2', genre: 'horror' };
+    const movie = await Movie.insert({ name: 'The Ring', genre: 'horror' });
+    const res = await request(app).put(`/api/v1/movies/${movie.id}`).send(newMovie);
+
+    expect(res.body).toEqual({ id: '1', ...newMovie });
   });
 });
